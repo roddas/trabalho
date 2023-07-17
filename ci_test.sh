dc1f5bdb26eff7fa75a4fe28c46224d175fd236f
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-07-16 23:23:57 -0300

    add the command to run all the tests

--- ci_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-cat "RAILS_ENV=test" >> .env
-docker compose up  -d decidim_web postgres
-docker exec -it decidim_web bash
-# bundle exec rails r test/test_helper.rb  
-cat test/test_helper.rb  | rails c
