3908cc23f456e3716acf8c7ed6fae34c80a36cf5
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-07-16 22:39:51 -0300

    add linter

--- ci_test.sh
+++ ci_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cat "RAILS_ENV=test" >> .env
-docker compose up -d
+docker compose up  -d decidim_web postgres
 docker exec -it decidim_web bash
 # bundle exec rails r test/test_helper.rb  
-ruby -I test test/test_helper.rb 
+cat test/test_helper.rb  | rails c
